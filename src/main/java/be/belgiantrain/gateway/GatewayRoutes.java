ffc1ac372d29dfed52f2869291cc6de0ea8520db
Couck Michael <michael.couck@belgiantrain.be>
2022-03-10 12:05:38 +0100

    Some small changes.

--- src/main/java/be/belgiantrain/gateway/GatewayRoutes.java
+++ src/main/java/be/belgiantrain/gateway/GatewayRoutes.java
@@ -1,36 +1,19 @@
 package be.belgiantrain.gateway;
 
 import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @RefreshScope
 @Configuration
-@SuppressWarnings({"DanglingJavadoc", "unused"})
+@SuppressWarnings({"unused"})
 public class GatewayRoutes {
 
-    /**
-     * <pre>
-     *     return builder.routes()
-     *                 .route(r -> r.path("/service/**").uri("lb://mySimpleService"))
-     *                 .build();
-     * </pre>
-     */
-    /*@Bean
-    @SuppressWarnings({"SpringJavaInjectionPointsAutowiringInspection", "Convert2Lambda"})
+    @Bean
     public RouteLocator routeLocator(final RouteLocatorBuilder builder) {
-        Function<PredicateSpec, Route.AsyncBuilder> all = new Function<PredicateSpec, Route.AsyncBuilder>() {
-            @Override
-            public Route.AsyncBuilder apply(final PredicateSpec predicateSpec) {
-                index = index == elasticUris.length ? 0 : index++;
-                String elasticUri = elasticUris[index];
-                return predicateSpec
-                        .path("/elastic/**") // query-service
-                        .filters(f -> f.stripPrefix(1))
-                        .uri(elasticUri); // http://sl01757v.ops.vdab.be:9200, lb://query-service
-                // "http://sl01757v.ops.vdab.be:9200"
-            }
-        };
-        return builder.routes().route(all).build();
-    }*/
+        return builder.routes().build();
+    }
 
 }
